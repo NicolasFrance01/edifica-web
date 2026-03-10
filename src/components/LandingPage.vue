caa801990edadaf90eafa297bd777a6dfdbb4354
NicolasFrance01 <francenicolas.dw@gmail.com>
2026-03-10 14:59:47 -0300

    Update Instagram link to @edifica.obras

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -122,7 +122,7 @@ const scrollToCalculator = () => {
                 </a>
               </li>
               <li class="social-item">
-                <a href="https://www.instagram.com/" target="_blank" class="social-link instagram" aria-label="Instagram">
+                <a href="https://www.instagram.com/edifica.obras/" target="_blank" class="social-link instagram" aria-label="Instagram">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
                 </a>
               </li>
